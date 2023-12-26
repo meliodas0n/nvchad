e27202a1302ff3fc148adc59ebcd04fb5ed1f259
meliodas0n <mrunalnarayana@gmail.com>
2023-12-26 22:00:26 +0530

    removed .gitignore which got me my custom config back

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,31 @@
+---@type ChadrcConfig
+local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "ashes",
+  theme_toggle = { "ashes", "one_light" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+}
+
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
+
+vim.opt.number = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+
+-- Define the key mappings for <F2>, <F3>, and <F4>
+vim.api.nvim_set_keymap('n', '<F2>', ':bprevious<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F3>', ':bnext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F4>', ':bd<CR>', { noremap = true, silent = true })
+
+return M
