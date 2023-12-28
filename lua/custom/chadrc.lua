c60b5be5e938e4141cc20535ac47aa71194d47df
meliodas0n <mrunalnarayana@gmail.com>
2023-12-29 01:20:24 +0530

    cleaned up, removed transparency doesn't work with ashes

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -24,20 +24,10 @@ vim.api.nvim_set_keymap("n", "<F2>", ":bprevious<CR>", { noremap = true, silent 
 vim.api.nvim_set_keymap("n", "<F3>", ":bnext<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<F4>", ":bd<CR>", { noremap = true, silent = true })
 
--- Map leader + [ to move indent left
+-- Indentation using leader[ and leader]
 vim.api.nvim_set_keymap("n", "<leader>[", "<<", { noremap = true })
--- Map leader + ] to move indent right
 vim.api.nvim_set_keymap("n", "<leader>]", ">>", { noremap = true })
--- Map leader + [ in visual mode to indent left
 vim.api.nvim_set_keymap("v", "<leader>[", "<gv", { noremap = true })
--- Map leader + ] in visual mode to indent right
 vim.api.nvim_set_keymap("v", "<leader>]", ">gv", { noremap = true })
-
--- Set transparency for floating windows (requires Neovim 0.5+)
-vim.cmd "highlight NormalFloat guibg=none" -- Make floating windows transparent
-vim.cmd "highlight FloatBorder guifg=none" -- Optional: Make float borders transparent
-
--- Set the transparency level using winblend
-vim.cmd "autocmd FileType * setlocal winblend=10" -- Adjust the transparency level (0-100)
 
 return M
