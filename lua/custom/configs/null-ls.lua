9f17ec6df902aa18120c7bd3dde077e154fb1b04
meliodas0n <mrunalnarayana@gmail.com>
2023-12-31 03:24:30 +0530

    added json, js, jsx, ts, tsx to prettier

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -7,7 +7,7 @@ local sources = {
 
   -- webdev stuff
   b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+  b.formatting.prettier.with { filetypes = { "html", "markdown", "css", "json", "tsx", "jsx", "ts", "js" } }, -- so prettier works only on these filetypes
 
   -- Lua
   b.formatting.stylua,
@@ -17,22 +17,22 @@ local sources = {
 
   -- python
   b.diagnostics.ruff,
-  b.formatting.black
+  b.formatting.black,
 }
 
 null_ls.setup {
   debug = true,
   sources = sources,
   on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
-          end,
-        })
-      end
-    end,
+          vim.lsp.buf.format { bufnr = bufnr }
+        end,
+      })
+    end
+  end,
 }
