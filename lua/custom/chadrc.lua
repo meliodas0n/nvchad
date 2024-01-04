eabada68fe05844efb831ae65f26cc3b15d74eac
meliodas0n <mrunalnarayana@gmail.com>
2024-01-05 01:50:41 +0530

    well I regretted it already

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -9,42 +9,6 @@ M.ui = {
   hl_override = highlights.override,
   hl_add = highlights.add,
   transparency = false,
-
-  -- nvdash (dashboard)
-  nvdash = {
-    load_on_startup = true,
-
-    header = {
-      "           ▄ ▄                   ",
-      "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
-      "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
-      "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
-      "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
-      "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
-      "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
-      "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
-      "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
-    },
-
-    buttons = {
-      { "  Find File", "Spc f f", "Telescope find_files" },
-      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
-      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
-      { "  Bookmarks", "Spc m a", "Telescope marks" },
-      { "  Themes", "Spc t h", "Telescope themes" },
-      { "  Mappings", "Spc c h", "NvCheatsheet" },
-    },
-
-    cheatsheet = { theme = "grid" }, -- simple/grid
-
-    lsp = {
-      -- show function signatures i.e args as you type
-      signature = {
-        disabled = false,
-        silent = true, -- silences 'no signature help available' message from appearing
-      },
-    },
-  },
 }
 
 M.plugins = "custom.plugins"
